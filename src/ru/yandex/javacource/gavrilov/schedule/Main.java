44ee0122443f6ae5b5673871bd83544efef673ad
Dmitry Gavrilov <mrfritherrr@yandex.ru>
2024-09-18 18:47:46 +0300

    Исправить стиль кода

--- src/ru/yandex/javacource/gavrilov/schedule/Main.java
+++ src/ru/yandex/javacource/gavrilov/schedule/Main.java
@@ -18,20 +18,20 @@ public class Main {
         Epic epic1 = new Epic("Очистить пол", "Подмести и помыть пол", TaskStatus.NEW);
         int epicId = manager.addEpic(epic1);
         int epic1Id = epicId;
-        Subtask subtask1Epic1 = new Subtask("Подмести пол"
-                , "Подмести весь пол веником", TaskStatus.NEW, epicId);
+        Subtask subtask1Epic1 = new Subtask("Подмести пол",
+                "Подмести весь пол веником", TaskStatus.NEW, epicId);
         int subtask1Epic1Id = manager.addSubtask(subtask1Epic1);
 
-        Subtask subtask2Epic1 = new Subtask("Помыть пол"
-                , "Помыть весь пол тряпкой", TaskStatus.NEW, epicId);
+        Subtask subtask2Epic1 = new Subtask("Помыть пол",
+                "Помыть весь пол тряпкой", TaskStatus.NEW, epicId);
         int subtask2Epic1Id = manager.addSubtask(subtask2Epic1);
 
         Epic epic2 = new Epic("Включить свет", "Включить свет нажав на переключатель", TaskStatus.NEW);
         epicId = manager.addEpic(epic2);
         int epic2Id = epicId;
 
-        Subtask subtask1Epic2 = new Subtask("Нажать переключатель"
-                , "Нажать на переключатель чтобы включился свет", TaskStatus.NEW, epicId);
+        Subtask subtask1Epic2 = new Subtask("Нажать переключатель",
+                "Нажать на переключатель чтобы включился свет", TaskStatus.NEW, epicId);
         int subtask1Epic2Id = manager.addSubtask(subtask1Epic2);
 
         System.out.println(manager.getTasks() + "" + manager.getEpics() + manager.getSubtasks());
@@ -52,8 +52,8 @@ public class Main {
         }
 
         manager.getSubtaskById(subtask2Epic1Id);
-        Subtask subtask2Epic2 = new Subtask("Помыть пол"
-                , "Помыть весь пол тряпкой", TaskStatus.DONE, epicId, subtask2Epic1Id);
+        Subtask subtask2Epic2 = new Subtask("Помыть пол",
+                "Помыть весь пол тряпкой", TaskStatus.DONE, epicId, subtask2Epic1Id);
         manager.updateSubtask(subtask2Epic2);
         subtask2Epic1.setId(subtask2Epic1Id);
 
